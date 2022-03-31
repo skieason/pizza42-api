8f350a77c1945cda66ded3344781d5f9192040fd
Phil Varner <philvarnerlp9@gmail.com>
2022-03-31 08:27:12 -1000

    test api deploy

--- api-server.js
+++ api-server.js
@@ -75,7 +75,7 @@ app.post('/api/:user_id/order/', checkJwt, checkScopes, (req, res) => {
 
 });
 
-const port = process.env.API_SERVER_PORT || 3001;
+// const port = process.env.API_SERVER_PORT || 3001;
 
-app.listen(port, () => console.log(`Api started on port ${port}`));
+app.listen(process.env.API_SERVER_PORT || 3001, () => console.log(`Api started...`));
 
